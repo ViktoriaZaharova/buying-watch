edb77ea47b24dac8de7c115e0868b2c2026faac7
Viktoria Zaharova <zaharova181114@gmail.com>
2024-10-18 00:18:12 +0300

    + 1 page

--- js/common.js
+++ js/common.js
@@ -1,2 +1,10 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
+$('.header-bottom .btn-close').on('click', function(e){
+  e.preventDefault();
+  $('.header-bottom').fadeOut();
+  });
+
+$('.btn-burger').on('click', function(){
+$('.header-bottom').fadeToggle();
+});
