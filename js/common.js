fe0899a7240b125a0792572ae3a6b9f1fcb16507
Viktoria Zaharova <zaharova181114@gmail.com>
2024-10-20 13:01:46 +0300

    index mobile

--- js/common.js
+++ js/common.js
@@ -1,10 +1,42 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
-$('.header-bottom .btn-close').on('click', function(e){
+$('.header-bottom .btn-close').on('click', function (e) {
   e.preventDefault();
   $('.header-bottom').fadeOut();
-  });
+});
 
-$('.btn-burger').on('click', function(){
-$('.header-bottom').fadeToggle();
+$('.btn-burger').on('click', function () {
+  $('.header-bottom').fadeToggle();
+});
+
+
+$('.brands-slider').slick({
+  slidesToShow: 6,
+  arrows: false,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        arrows: false,
+        dots: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
